e302e189be414968aa75a4458252ebe6122e70f2
nitrocaster <paul-kv@yandex.ru>
2015-04-01 12:04:06 +0300

    Add solution with stub projects.

---
+++ src/server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SdMessenger
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
