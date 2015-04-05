451ea3842197636464ead41f26bc912727175258
nitrocaster <paul-kv@yandex.ru>
2015-04-06 00:56:59 +0300

    Add wrappers for JsonTextReader and JsonTextWriter.

---
+++ src/core/Common/JsonUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace Sdm.Core.Json
+{
+    public class JsonStreamWriter : JsonTextWriter
+    {
+        public JsonStreamWriter(Stream s)
+            : base(new StreamWriter(s))
+        {}
+    }
+
+    public class JsonStreamReader : JsonTextReader
+    {
+        public JsonStreamReader(Stream s)
+            : base(new StreamReader(s))
+        {}
+    }
+}
