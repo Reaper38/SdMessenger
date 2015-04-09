64431b91bd0986cbf5e1a8b72bd20900afdd6d7f
nitrocaster <paul-kv@yandex.ru>
2015-04-10 02:15:02 +0300

    Add UnbufferedStreamReader class.

---
+++ src/core/IO/UnbufferedStreamReader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Sdm.Core.IO
+{
+    public class UnbufferedStreamReader : TextReader
+    {
+        private Stream stream;
+        private BinaryReader r;
+        private bool own;
+        private bool disposed = false;
+
+        public UnbufferedStreamReader(Stream stream, Encoding encoding, bool own)
+        {
+            this.stream = stream;
+            this.own = own;
+            r = new BinaryReader(stream, encoding);
+        }
+
+        public UnbufferedStreamReader(Stream stream) :
+            this(stream, Encoding.Default, true)
+        { }
+
+        public bool EndOfStream
+        {
+            get
+            {
+                if (disposed)
+                    throw new ObjectDisposedException("stream");
+                // todo: check if there's async task in progress (see StreamReader impl)
+                return stream.Position >= stream.Length;
+            }
+        }
+
+        public override int Read() { return EndOfStream ? -1 : r.ReadChar(); }
+
+        public override int Peek() { return r.PeekChar(); }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+            if (disposing)
+            {
+                if (own)
+                    r.Close();
+            }
+            disposed = true;
+        }
+
+        ~UnbufferedStreamReader() { Dispose(false); }
+    }
+}
