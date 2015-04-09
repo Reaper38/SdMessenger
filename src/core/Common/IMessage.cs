d4b38d05d18631c39422a576dae1faf3998ea8a4
nitrocaster <paul-kv@yandex.ru>
2015-04-10 02:57:13 +0300

    Decompose IMessage and IMessageHeader.

--- src/core/Common/IMessage.cs
+++ src/core/Common/IMessage.cs
@@ -1,13 +1,32 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace Sdm.Core
 {
-    public interface IMessage
+    public interface ISdmSerializable
     {
-        MessageId Id { get; }
         // Stream must not be closed/disposed here
         void Load(Stream s, ProtocolId ptype);
         // Stream must not be closed/disposed here
         void Save(Stream s, ProtocolId ptype);
+    }
+
+    public interface IMessage : ISdmSerializable
+    {
+        MessageId Id { get; }
+    }
+
+    [Flags]
+    public enum MessageFlags : byte
+    {
+        None = 0,
+        Secure = 1,
+    }
+
+    public interface IMessageHeader : ISdmSerializable
+    {
+        int Size { get; }
+        MessageFlags Flags { get; }
+        MessageId Id { get; }
     }
 }
