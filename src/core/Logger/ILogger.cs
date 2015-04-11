cc568983bee99be2d468d06d3e5d318ca0dafba8
nitrocaster <paul-kv@yandex.ru>
2015-04-11 15:34:34 +0300

    Add logger interface.

---
+++ src/core/Logger/ILogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sdm.Core
+{
+    public enum LogLevel
+    {
+        Trace,
+        Debug,
+        Info,
+        Warning,
+        Error,
+        Fatal
+    }
+
+    public interface ILogger : IDisposable
+    {
+        event Action<string> MessageLogged;
+        event Action LogCleared;
+        int LineCount { get; }
+        void Log(LogLevel logLevel, string message);
+        void Clear();
+        void Flush();
+    }
+}
