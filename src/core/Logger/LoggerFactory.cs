09511a7ac5dbb23a55ebd691b95752fbeef3f805
nitrocaster <paul-kv@yandex.ru>
2015-04-11 17:08:20 +0300

    Add logger factory stub.

---
+++ src/core/Logger/LoggerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sdm.Core
+{
+    public sealed class LoggerFactory
+    {
+        #region Singleton implementation
+
+        private LoggerFactory() {}
+        private static readonly LoggerFactory instance = new LoggerFactory();
+        public static LoggerFactory Instance { get { return instance; } }
+
+        #endregion
+
+        public ILogger CreateLogger(object user, LogLevel lvl)
+        {
+            // todo: implement CreateLogger
+            /* supposed implementation:
+            if (user is IServer)
+                return new ServerLogger("sdm_server.log", lvl);
+            if (user is IClient)
+                return new ClientLogger(".", "sdm_client", ".log", lvl);
+            throw new NotSupportedException("There's no logger provided for specified user.");
+            */
+            throw new NotImplementedException();
+        }
+    }
+}
