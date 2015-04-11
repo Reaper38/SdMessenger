7117c75664af6d73cebb8d7bbfa0e32668a8bfbc
nitrocaster <paul-kv@yandex.ru>
2015-04-12 01:06:03 +0300

    Update ICryptoProvider, introduce derived sym/asym cryptoprovider interfaces.

--- src/core/Crypto/ICryptoProvider.cs
+++ src/core/Crypto/ICryptoProvider.cs
@@ -1,13 +1,41 @@
-﻿namespace Sdm.Core
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Sdm.Core
 {
     public interface ICryptoProvider
     {
-        byte[] GetPrivateKeyASym();
-        byte[] GetPublicKeyASym();
-        string EncryptASym(string plaintext, string publicKey_ = null);
-        string DecryptASym(string ciphertext, string privateKey_ = null);
-        string EncryptSym(string input, string password, byte[] saltBytes);
-        string DecryptSym(string input, string password, byte[] saltBytes);
+        /// <summary>
+        /// Gets valid key sizes (in bits).
+        /// </summary>
+        IEnumerable<int> ValidKeySizes { get; }
+        /// <summary>
+        /// Initializes ICryptoProvider instance using given key size.
+        /// </summary>
+        /// <param name="keySize">Key size (in bits)</param>
+        void Initialize(int keySize);
+        /// <summary>
+        /// Returns size of encrypted data.
+        /// </summary>
+        /// <param name="noncryptedSize">Size of reference (non-crypted) data.</param>
+        int ComputeEncryptedSize(int noncryptedSize);
+        /// <summary>
+        /// Returns size of decrypted data.
+        /// </summary>
+        /// <param name="encryptedSize">Size of encrypted data.</param>
+        int ComputeDecryptedSize(int encryptedSize);
+        void Encrypt(Stream dst, Stream src, int srcByteCount);
+        void Decrypt(Stream dst, Stream src, int srcByteCount);
+    }
 
+    public interface ISymmetricCryptoProvider : ICryptoProvider
+    {
+        byte[] Key { get; set; }
+    }
+    
+    public interface IAsymmetricCryptoProvider : ICryptoProvider
+    {
+        string GetKey(bool includePrivateParams = false);
+        void SetKey(string key);
     }
 }
