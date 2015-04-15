524ccfefcd40309a9a572e2ad799fe793fd142ae
nitrocaster <paul-kv@yandex.ru>
2015-04-16 02:08:54 +0300

    Transform MessageIdRegistry into IMessage factory.

--- src/core/Common/MessageIdChecker.cs
+++ src/core/Common/MessageIdChecker.cs
@@ -4,44 +4,49 @@ using System.IO;
 
 namespace Sdm.Core
 {
-    namespace Detail
+    public class MessageFactory
     {
-        internal static class MessageIdRegistry
-        {
-            private static Dictionary<MessageId, Type> dict = new Dictionary<MessageId, Type>();
-            private static Dictionary<Type, MessageId> rdict = new Dictionary<Type, MessageId>();
-            private static object sync = new object();
+        private static Dictionary<MessageId, Type> dict = new Dictionary<MessageId, Type>();
+        private static Dictionary<Type, MessageId> rdict = new Dictionary<Type, MessageId>();
+        private static object sync = new object();
 
-            public static void Register(MessageId newMsgId, Type newType)
+        internal static void Register(MessageId newMsgId, Type newType)
+        {
+            lock (sync)
             {
-                lock (sync)
+                if (dict.ContainsKey(newMsgId))
                 {
-                    if (dict.ContainsKey(newMsgId))
+                    var currentType = dict[newMsgId];
+                    if (currentType != newType)
                     {
-                        var currentType = dict[newMsgId];
-                        if (currentType != newType)
-                        {
-                            throw new Exception(String.Format("Can't associate {0}.{1} with type {2} " +
-                                "because it is already associated with type {3}.",
-                                typeof(MessageId).FullName, newMsgId, newType.FullName, currentType.FullName));
-                        }
+                        throw new Exception(String.Format("Can't associate {0}.{1} with type {2} " +
+                            "because it is already associated with type {3}.",
+                            typeof(MessageId).FullName, newMsgId, newType.FullName, currentType.FullName));
                     }
-                    else
-                        dict.Add(newMsgId, newType);
-                    if (rdict.ContainsKey(newType))
-                    {
-                        var currentMsgId = rdict[newType];
-                        if (currentMsgId != newMsgId)
-                        {
-                            throw new Exception(String.Format("Can't associate type {0} with {1}.{2} " +
-                                "because it is already associated with {1}.{3}.",
-                                newType.FullName, typeof(MessageId).FullName, newMsgId, currentMsgId));
-                        }
-                    }
-                    else
-                        rdict.Add(newType, newMsgId);
                 }
+                else
+                    dict.Add(newMsgId, newType);
+                if (rdict.ContainsKey(newType))
+                {
+                    var currentMsgId = rdict[newType];
+                    if (currentMsgId != newMsgId)
+                    {
+                        throw new Exception(String.Format("Can't associate type {0} with {1}.{2} " +
+                            "because it is already associated with {1}.{3}.",
+                            newType.FullName, typeof(MessageId).FullName, newMsgId, currentMsgId));
+                    }
+                }
+                else
+                    rdict.Add(newType, newMsgId);
             }
+        }
+
+        public static IMessage CreateMessage(MessageId id)
+        {
+            Type type;
+            if (!dict.TryGetValue(id, out type))
+                throw new InvalidOperationException(String.Format("Type with id '{0}' is not registered", id));
+            return (IMessage)Activator.CreateInstance(type);
         }
     }
 
@@ -51,7 +56,7 @@ namespace Sdm.Core
 
         protected MessageIdChecker(MessageId msgId)
         {
-            Detail.MessageIdRegistry.Register(msgId, GetType());
+            MessageFactory.Register(msgId, GetType());
             id = msgId;
         }
 
