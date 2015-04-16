5e72ef491d59a5ef840eaaf4bb1fb2d2a0b50858
nitrocaster <paul-kv@yandex.ru>
2015-04-17 00:20:02 +0300

    Add Stream and BinaryReader extensions.

---
+++ src/core/Util/StreamUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Sdm.Core.Util
+{
+    public static class StreamUtil
+    {
+        public static int ReadTo(this Stream src, Stream dst, int count)
+        {
+            var buf = new byte[256];
+            var readByteCount = 0;
+            while (readByteCount < count)
+            {
+                var r = src.Read(buf, 0, Math.Min(buf.Length, count - readByteCount));
+                if (r == 0)
+                    break;
+                dst.Write(buf, 0, r);
+                readByteCount += r;
+            }
+            return readByteCount;
+        }
+
+        public static int ReadTo(this BinaryReader src, Stream dst, int count)
+        {
+            var buf = new byte[256];
+            var readByteCount = 0;
+            while (readByteCount < count)
+            {
+                var r = src.Read(buf, 0, Math.Min(buf.Length, count - readByteCount));
+                if (r == 0)
+                    break;
+                dst.Write(buf, 0, r);
+                readByteCount += r;
+            }
+            return readByteCount;
+        }
+    }
+}
