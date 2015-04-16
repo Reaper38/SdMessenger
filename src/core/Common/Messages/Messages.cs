1935346eea7cdaaf7428b6813725ce1670c6d5d3
nitrocaster <paul-kv@yandex.ru>
2015-04-16 04:09:33 +0300

    Add dataless message base class.

---
+++ src/core/Common/Messages/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Sdm.Core.Messages
+{
+    // for messages without data
+    public class DummyMessage<T> : MessageIdChecker<T>
+    {
+        protected DummyMessage(MessageId id) : base(id) {}
+        public override void Load(Stream s, ProtocolId ptype) {}
+        public override void Save(Stream s, ProtocolId ptype) {}
+    }
+}
