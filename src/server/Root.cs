fca0a2c99c80e58bc65abdc275736d42a46b4980
nitrocaster <paul-kv@yandex.ru>
2015-04-17 23:54:03 +0300

    Add logger initialization and proxy Root.Log() method.

--- src/server/Root.cs
+++ src/server/Root.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Sdm.Core;
 
 namespace Sdm.Server
 {
     internal static class Root
     {
-        static int Main(string[] args)
+        private static ILogger logger;
+
+        public static void Log(LogLevel lvl, string msg) { logger.Log(lvl, msg); }
+
+        private static int Main(string[] args)
         {
+            using (logger = LoggerFactory.Instance.CreateLogger(LoggerType.Server, LogLevel.Trace))
+            {
+                // XXX: initialize server here
+            }
             return 0;
         }
     }
