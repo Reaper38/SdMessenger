e8adf2f52957f200a1143f1f0d495e95863dc0f3
nitrocaster <paul-kv@yandex.ru>
2015-04-17 14:34:50 +0300

    Add cryptoprovider factory.

---
+++ src/core/Crypto/CryptoProviderFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Sdm.Core
+{
+    public enum SdmSymmetricAlgorithm
+    {
+        AES,
+    }
+
+    public enum SdmAsymmetricAlgorithm
+    {
+        RSA,
+    }
+
+    public sealed class CryptoProviderFactory
+    {
+        #region Singleton implementation
+
+        private CryptoProviderFactory() {}
+        private static readonly CryptoProviderFactory instance = new CryptoProviderFactory();
+        public static CryptoProviderFactory Instance { get { return instance; } }
+
+        #endregion
+
+        public ISymmetricCryptoProvider CreateSymmetric(SdmSymmetricAlgorithm algorithm)
+        {
+            switch (algorithm)
+            {
+            case SdmSymmetricAlgorithm.AES: return new AESCryptoProvider();
+            default: throw new NotSupportedException(algorithm + " is not supported.");
+            }
+        }
+
+        public IAsymmetricCryptoProvider CreateAsymmetric(SdmAsymmetricAlgorithm algorithm)
+        {
+            switch (algorithm)
+            {
+            case SdmAsymmetricAlgorithm.RSA: return new RSACryptoProvider();
+            default: throw new NotSupportedException(algorithm + " is not supported.");
+            }
+        }
+    }
+}
