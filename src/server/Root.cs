50fa7b530bf242e3fd6f33c30214602a3dc120d9
nitrocaster <paul-kv@yandex.ru>
2015-04-17 23:59:44 +0300

    Add Root.Log() overload.

--- src/server/Root.cs
+++ src/server/Root.cs
@@ -12,6 +12,9 @@ namespace Sdm.Server
 
         public static void Log(LogLevel lvl, string msg) { logger.Log(lvl, msg); }
 
+        public static void Log(LogLevel lvl, string format, params object[] args)
+        { logger.Log(lvl, String.Format(format, args)); }
+
         private static int Main(string[] args)
         {
             using (logger = LoggerFactory.Instance.CreateLogger(LoggerType.Server, LogLevel.Trace))
