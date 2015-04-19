7f9eb60730f8d9ffe88585a5c30369bae05b133a
nitrocaster <paul-kv@yandex.ru>
2015-04-20 01:34:23 +0300

    Initialize and destroy client's core.

--- src/client/Root.cs
+++ src/client/Root.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Sdm.Core;
 
 namespace Sdm.Client
 {
@@ -10,9 +11,11 @@ namespace Sdm.Client
         [STAThread]
         private static void Main()
         {
+            SdmCore.Initialize(AppType.Client);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run();
+            SdmCore.Destroy();
         }
     }
 }
