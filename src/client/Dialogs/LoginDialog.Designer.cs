8f50d806502d1b1dcaf1d7db361234638a5c0626
nitrocaster <paul-kv@yandex.ru>
2015-04-20 01:54:18 +0300

    Add login dialog.

---
+++ src/client/Dialogs/LoginDialog.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Sdm.Client
+{
+    partial class LoginDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ttAlert = new System.Windows.Forms.ToolTip(this.components);
+            this.lPassword = new System.Windows.Forms.Label();
+            this.lLogin = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lHost = new System.Windows.Forms.Label();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.chkSavePass = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lPassword
+            // 
+            this.lPassword.AutoSize = true;
+            this.lPassword.Location = new System.Drawing.Point(12, 87);
+            this.lPassword.Name = "lPassword";
+            this.lPassword.Size = new System.Drawing.Size(53, 13);
+            this.lPassword.TabIndex = 0;
+            this.lPassword.Text = "Password";
+            // 
+            // lLogin
+            // 
+            this.lLogin.AutoSize = true;
+            this.lLogin.Location = new System.Drawing.Point(12, 48);
+            this.lLogin.Name = "lLogin";
+            this.lLogin.Size = new System.Drawing.Size(33, 13);
+            this.lLogin.TabIndex = 0;
+            this.lLogin.Text = "Login";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(12, 103);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(180, 20);
+            this.tbPassword.TabIndex = 4;
+            this.tbPassword.Text = "qwerty";
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(12, 64);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(180, 20);
+            this.tbLogin.TabIndex = 3;
+            this.tbLogin.Text = "admin";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.Transparent;
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnConnect.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnConnect.Location = new System.Drawing.Point(12, 152);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(180, 24);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            // 
+            // lHost
+            // 
+            this.lHost.AutoSize = true;
+            this.lHost.Location = new System.Drawing.Point(12, 9);
+            this.lHost.Name = "lHost";
+            this.lHost.Size = new System.Drawing.Size(51, 13);
+            this.lHost.TabIndex = 0;
+            this.lHost.Text = "Host:Port";
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(12, 25);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(180, 20);
+            this.tbHost.TabIndex = 2;
+            // 
+            // chkSavePass
+            // 
+            this.chkSavePass.AutoSize = true;
+            this.chkSavePass.Location = new System.Drawing.Point(93, 130);
+            this.chkSavePass.Name = "chkSavePass";
+            this.chkSavePass.Size = new System.Drawing.Size(99, 17);
+            this.chkSavePass.TabIndex = 5;
+            this.chkSavePass.Text = "Save password";
+            this.chkSavePass.UseVisualStyleBackColor = true;
+            // 
+            // LoginDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 188);
+            this.Controls.Add(this.chkSavePass);
+            this.Controls.Add(this.lPassword);
+            this.Controls.Add(this.lLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lHost);
+            this.Controls.Add(this.tbHost);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "LoginDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SdmClient - Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.ToolTip ttAlert;
+        private System.Windows.Forms.Label lPassword;
+        private System.Windows.Forms.Label lLogin;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lHost;
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.CheckBox chkSavePass;
+    }
+}
