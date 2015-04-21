4d52e4cbdaf53fcb1e17ca4947c6c3c5b9207f45
nitrocaster <paul-kv@yandex.ru>
2015-04-21 21:57:46 +0300

    Add extended RichTextBox control.

---
+++ src/client/Controls/RichTextBoxEx.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace Sdm.Client.Controls
+{
+    internal class RichTextBoxEx : RichTextBox
+    {
+        public void AppendRtf(string rtf)
+        {
+            var s = SelectionStart;
+            var l = SelectionLength;
+            Select(TextLength, 0);
+            SelectedRtf = rtf;
+            Select(s, l);
+        }
+    }
+}
