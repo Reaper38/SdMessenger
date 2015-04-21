1fcac4368df511718289ae30578281b6dd4d42cc
nitrocaster <paul-kv@yandex.ru>
2015-04-21 22:00:54 +0300

    Add rtf utility class.

---
+++ src/client/Util/RtfUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Sdm.Client.Util
+{
+    public static class RtfUtil
+    {
+        public static void EscapeString(StringBuilder dst, string src)
+        {
+            for (int i = 0; i < src.Length; i++)
+            {
+                var c = src[i];
+                switch (c)
+                {
+                case '\\': dst.Append(@"\\"); continue;
+                case '{': dst.Append(@"\{"); continue;
+                case '}': dst.Append(@"\}"); continue;
+                }
+                if (c <= 0x7f)
+                    dst.Append(c);
+                else
+                    dst.Append(@"\u").Append((uint)c).Append('?');
+            }
+        }
+    }
+}
