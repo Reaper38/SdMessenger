273f08d6348beff340a344059d94afc21986f36c
nitrocaster <paul-kv@yandex.ru>
2015-04-22 11:00:05 +0300

    Convert pure client interface into abstract class.

--- src/core/Common/IPureClient.cs
+++ src/core/Common/IPureClient.cs
@@ -8,18 +8,32 @@ using System.Text;
 namespace Sdm.Core
 {
     // represents local client (for client app)
-    public interface IPureClient
+    public abstract class PureClientBase : IDisposable
     {
-        event Action<ConnectionResult> ConnectionResult;
+        public event Action<ConnectionResult> ConnectionResult;
 
-        ConnectionState ConnectionState { get; }
-        ClientId Id { get; }
-        IPAddress ServerAddress { get; }
-        ushort ServerPort { get; }
-        INetStatistics Stats { get; }
+        public abstract ConnectionState ConnectionState { get; }
+        public abstract ClientId Id { get; }
+        public abstract IPAddress ServerAddress { get; }
+        public abstract ushort ServerPort { get; }
+        public abstract INetStatistics Stats { get; }
         // connection result have to be checked using ConnectionResult event
-        void Connect(IPAddress address, ushort port, string login, string password);
-        void Disconnect();
-        void OnMessage(IMessage msg);
+        public abstract void Connect(IPAddress address, ushort port, string login, string password);
+        public abstract void Disconnect();
+        public abstract void OnMessage(IMessage msg);
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected abstract void Dispose(bool disposing);
+
+        ~PureClientBase() { Dispose(false); }
+
+        #endregion
     }
 }
