17478ea1e1d462daf0fc7a1e23150cefdd9e4fae
nitrocaster <paul-kv@yandex.ru>
2015-04-25 03:01:53 +0300

    Add user account subsystem.

---
+++ src/server/UserAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Sdm.Core;
+
+namespace Sdm.Server
+{
+    public sealed class UserAccount
+    {
+        private static SHA256 sha = SHA256.Create(); // XXX: dispose?
+        private const string salt = "zx$$";
+
+        public string Login { get; private set; }
+        /// <summary>Base64 string with SHA-256 hash of password+salt</summary>
+        public string Password { get; private set; }
+        public ClientAccessFlags Access { get; set; }
+
+        public UserAccount(string login, string passwordHash, ClientAccessFlags access)
+        {
+            Login = login;
+            Password = passwordHash;
+            Access = access;
+        }
+
+        public static string TransformPassword(string plainPassword)
+        {
+            var hash = sha.ComputeHash(Encoding.UTF8.GetBytes(plainPassword + salt));
+            var shash = Convert.ToBase64String(hash);
+            return shash;
+        }
+
+        public bool VerifyPassword(string plainPassword)
+        {
+            var shash = TransformPassword(plainPassword);
+            return shash == Password;
+        }
+    }
+}
---
+++ src/server/UserList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Sdm.Core;
+
+namespace Sdm.Server
+{
+    internal sealed class UserList
+    {
+        private SortedList<string, UserAccount> users;
+
+        public UserList()
+        { users = new SortedList<string, UserAccount>(); }
+
+        public UserAccount FindUser(string login)
+        {
+            UserAccount acc;
+            return users.TryGetValue(login, out acc) ? acc : null;
+        }
+
+        public bool AddUser(UserAccount acc)
+        {
+            if (users.ContainsKey(acc.Login))
+                return false;
+            users.Add(acc.Login, acc);
+            return true;
+        }
+
+        public bool RemoveUser(string name)
+        {
+            if (!users.ContainsKey(name))
+                return false;
+            users.Remove(name);
+            return true;
+        }
+
+        public void Load(string filename)
+        {
+            var cfg = new IniFile(filename, Encoding.UTF8);
+            var userCount = cfg.GetSectionCount();
+            users.Capacity = userCount;
+            for (int i = 0; i < userCount; i++)
+            {
+                var sect = "user_" + i;
+                // XXX: handle errors
+                var login = cfg.GetString(sect, "login");
+                var password = cfg.GetString(sect, "password");
+                var access = (ClientAccessFlags)cfg.GetInt32(sect, "access");
+                var acc = new UserAccount(login, password, access);
+                users.Add(login, acc);
+            }
+        }
+
+        public void Save(string filename)
+        {
+            using (var w = new StreamWriter(filename, false, Encoding.UTF8))
+            {
+                int i = 0;
+                foreach (var kv in users)
+                {
+                    var acc = kv.Value;
+                    w.Write("[user_");
+                    w.Write(i);
+                    w.WriteLine(']');
+                    w.Write("login = ");
+                    w.WriteLine(acc.Login);
+                    w.Write("password = ");
+                    w.WriteLine(acc.Password);
+                    w.Write("access = ");
+                    w.WriteLine((int)acc.Access);
+                    i++;
+                }
+            }
+        }
+    }
+}
