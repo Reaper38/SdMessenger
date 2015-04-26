e0308a28fd8ef34e27e100b945783b852f13771e
nitrocaster <paul-kv@yandex.ru>
2015-04-26 13:45:59 +0300

    Add pipe server for handling external commands.

---
+++ src/server/PipeConsoleServer.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading;
+using Sdm.Core.Util;
+
+namespace Sdm.Server
+{
+    internal delegate void PipeConsoleCommandHandler(string command, PipeConsole console);
+
+    internal sealed class PipeConsoleServer : IDisposable
+    {
+        private bool disposed = false;
+        private readonly NamedPipeServerStream svPipe;
+        private readonly StreamWriter svPipeWriter;
+        private readonly object readerLock = 0;
+        private readonly PipeConsoleCommandHandler handler;
+
+        public const string PipeName = "sdm_server";
+        
+        public PipeConsoleServer(PipeConsoleCommandHandler handler)
+        {
+            this.handler = handler;
+            svPipe = new NamedPipeServerStream(PipeName, PipeDirection.InOut, 1,
+                PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+            svPipeWriter = new StreamWriter(svPipe, Encoding.ASCII);
+            svPipe.BeginWaitForConnection(OnConnection, null);
+        }
+        
+        private void OnConnection(IAsyncResult ar)
+        {
+            svPipe.EndWaitForConnection(ar);
+            if (!svPipe.IsConnected)
+                return;
+            var thread = new Thread(ClientServiceLoop);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void OnTempPipeConnection(IAsyncResult ar)
+        {
+            var locker = ar.AsyncState;
+            lock (locker)
+            { Monitor.Pulse(locker); }
+        }
+
+        private void ClientServiceLoop()
+        {
+            var tmpPipeName = String.Format("{0}_tmp_{1}", PipeName, (ulong)DateTime.Now.ToBinary());
+            lock (readerLock)
+            {
+                svPipeWriter.WriteLine(tmpPipeName);
+                svPipeWriter.Flush();
+                svPipe.WaitForPipeDrain();
+                svPipe.Disconnect();
+                // continue waiting
+                svPipe.BeginWaitForConnection(OnConnection, null);
+            }
+            using (var pipe = new NamedPipeServerStream(tmpPipeName, PipeDirection.InOut, 1,
+                PipeTransmissionMode.Message, PipeOptions.Asynchronous))
+            {
+                object locker = 1;
+                lock (locker)
+                {
+                    var ar = pipe.BeginWaitForConnection(OnTempPipeConnection, locker);
+                    var timedOut = !Monitor.Wait(locker, 1000);
+                    pipe.EndWaitForConnection(ar);
+                    if (timedOut || !pipe.IsConnected)
+                        return;
+                }
+                var clReader = new StreamReader(pipe, true);
+                var clWriter = new StreamWriter(pipe, Encoding.UTF8);
+                var proxy = new PipeConsole(clReader, clWriter);
+                // fetch command from pipe while connected
+                var cmd = clReader.ReadLine();
+                if (cmd != null)
+                    handler(cmd, proxy);
+                pipe.WaitForPipeDrain();
+                pipe.Disconnect();
+            }
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    svPipe.Disconnect();
+                    svPipeWriter.Dispose();
+                }
+                DisposeHelper.OnDispose<PipeConsoleServer>(disposing);
+                disposed = true;
+            }
+        }
+
+        ~PipeConsoleServer() { Dispose(false); }
+
+        #endregion
+    }
+
+    internal sealed class PipeConsole
+    {
+        private readonly StreamReader reader; // reserved for future use
+        private readonly StreamWriter writer;
+
+        public PipeConsole(StreamReader reader, StreamWriter writer)
+        {
+            this.reader = reader;
+            this.writer = writer;
+        }
+        
+        public void WriteLine(string line)
+        {
+            writer.WriteLine(line);
+            writer.Flush();
+        }
+
+        public void WriteLine(string format, params object[] args)
+        {
+            writer.WriteLine(format, args);
+            writer.Flush();
+        }
+    }
+}
--- src/server/Root.cs
+++ src/server/Root.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
 using System.Threading;
 using Sdm.Core;
 
@@ -12,28 +15,104 @@ namespace Sdm.Server
 
         public static void Log(LogLevel lvl, string format, params object[] args)
         { SdmCore.Logger.Log(lvl, String.Format(format, args)); }
+
+        private static int SendCommand(string[] args)
+        {
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                mutex.ReleaseMutex();
+                Console.WriteLine("error: server is not running");
+                return -1;
+            }
+            var command = args[0];
+            string tmpPipeName = null;
+            using (var pipe = new NamedPipeClientStream(".", PipeConsoleServer.PipeName, PipeDirection.InOut))
+            {
+                try
+                { pipe.Connect(1000); }
+                catch (TimeoutException)
+                {
+                    Console.WriteLine("error: connection timed out");
+                    return -1;
+                }
+                var reader = new StreamReader(pipe, true);
+                tmpPipeName = reader.ReadLine();
+            }
+            // next, create new pipe to send/receive commands
+            using (var clPipe = new NamedPipeClientStream(".", tmpPipeName, PipeDirection.InOut))
+            {
+                try
+                { clPipe.Connect(1000); }
+                catch (TimeoutException)
+                {
+                    Console.WriteLine("error: connection timed out");
+                    return -1;
+                }
+                var clReader = new StreamReader(clPipe, true);
+                var clWriter = new StreamWriter(clPipe, Encoding.UTF8);
+                // send initial command
+                clWriter.WriteLine(command);
+                clWriter.Flush();
+                // fetch all available data from pipe
+                while (true)
+                {
+                    var reply = clReader.ReadLine();
+                    if (reply == null)
+                        break;
+                    Console.WriteLine(reply);
+                }
+            }
+            return 0;
+        }
+
+        private static void OnConsoleCommand(string command, PipeConsole console)
+        {
+            // XXX: add users/change settings/etc
+            console.WriteLine("not supported yet");
+        }
         
-        private static int Main(string[] args)
+        private static int RunServer(string[] args)
         {
             if (!mutex.WaitOne(TimeSpan.Zero, true))
             {
                 Console.WriteLine("Already running");
-                return 0;
+                return -1;
             }
             SdmCore.Initialize(AppType.Server);
-            var cfg = new ServerConfig();
-            using (var srv = new Server(cfg))
+            using (var pcs = new PipeConsoleServer(OnConsoleCommand))
             {
-                srv.Connect(cfg.Address, cfg.Port);
-                while (srv.Connected)
+                var cfg = new ServerConfig();
+                using (var srv = new Server(cfg))
                 {
-                    Thread.Sleep(cfg.UpdateSleep);
-                    srv.Update();
+                    srv.Connect(cfg.Address, cfg.Port);
+                    while (srv.Connected)
+                    {
+                        Thread.Sleep(cfg.UpdateSleep);
+                        srv.Update();
+                    }
                 }
             }
             SdmCore.Destroy();
             mutex.ReleaseMutex();
             return 0;
+        }
+
+        private static int PrintUsage(string[] args)
+        {
+            Console.WriteLine("Usage: <to be documented>");
+            return 0;
+        }
+
+        private static int Main(string[] args)
+        {
+            if (args.Length == 0)
+                return PrintUsage(args);
+            switch (args[0])
+            {
+            case "run": return RunServer(args);
+            case "help": return PrintUsage(args);
+            default: return SendCommand(args);
+            }
         }
     }
 }
