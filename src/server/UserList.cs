60621212f4a80d18c406aa1cd1039a9a67842665
nitrocaster <paul-kv@yandex.ru>
2015-04-26 13:50:59 +0300

    Make UserList threadsafe.

--- src/server/UserList.cs
+++ src/server/UserList.cs
@@ -14,6 +14,7 @@ namespace Sdm.Server
              password = NmU0ZTMzZjc0NDE5N2ZlZmE4Yjc2Y2U3NDJmOGUyM2M=
              access = 3           
          */
+        private readonly object sync = 1;
         private SortedList<string, UserAccount> users;
         public const string FileName = "sdm_server_users.ini";
 
@@ -22,42 +23,54 @@ namespace Sdm.Server
 
         public UserAccount FindUser(string login)
         {
-            UserAccount acc;
-            return users.TryGetValue(login, out acc) ? acc : null;
+            lock (sync)
+            {
+                UserAccount acc;
+                return users.TryGetValue(login, out acc) ? acc : null;
+            }
         }
 
         public bool AddUser(UserAccount acc)
         {
-            if (users.ContainsKey(acc.Login))
-                return false;
-            users.Add(acc.Login, acc);
-            return true;
+            lock (sync)
+            {
+                if (users.ContainsKey(acc.Login))
+                    return false;
+                users.Add(acc.Login, acc);
+                return true;
+            }
         }
 
         public bool RemoveUser(string name)
         {
-            if (!users.ContainsKey(name))
-                return false;
-            users.Remove(name);
-            return true;
+            lock (sync)
+            {
+                if (!users.ContainsKey(name))
+                    return false;
+                users.Remove(name);
+                return true;
+            }
         }
 
         public void Load()
         {
             if (!File.Exists(FileName))
                 return;
-            var cfg = new IniFile(FileName, Encoding.UTF8);
-            var userCount = cfg.GetSectionCount();
-            users.Capacity = userCount;
-            for (int i = 0; i < userCount; i++)
+            lock (sync)
             {
-                var sect = "user_" + i;
-                // XXX: handle errors
-                var login = cfg.GetString(sect, "login");
-                var password = cfg.GetString(sect, "password");
-                var access = (ClientAccessFlags)cfg.GetInt32(sect, "access");
-                var acc = new UserAccount(login, password, access);
-                users.Add(login, acc);
+                var cfg = new IniFile(FileName, Encoding.UTF8);
+                var userCount = cfg.GetSectionCount();
+                users.Capacity = userCount;
+                for (int i = 0; i < userCount; i++)
+                {
+                    var sect = "user_" + i;
+                    // XXX: handle errors
+                    var login = cfg.GetString(sect, "login");
+                    var password = cfg.GetString(sect, "password");
+                    var access = (ClientAccessFlags) cfg.GetInt32(sect, "access");
+                    var acc = new UserAccount(login, password, access);
+                    users.Add(login, acc);
+                }
             }
         }
 
@@ -66,19 +79,22 @@ namespace Sdm.Server
             using (var w = new StreamWriter(FileName, false, Encoding.UTF8))
             {
                 int i = 0;
-                foreach (var kv in users)
+                lock (sync)
                 {
-                    var acc = kv.Value;
-                    w.Write("[user_");
-                    w.Write(i);
-                    w.WriteLine(']');
-                    w.Write("login = ");
-                    w.WriteLine(acc.Login);
-                    w.Write("password = ");
-                    w.WriteLine(acc.Password);
-                    w.Write("access = ");
-                    w.WriteLine((int)acc.Access);
-                    i++;
+                    foreach (var kv in users)
+                    {
+                        var acc = kv.Value;
+                        w.Write("[user_");
+                        w.Write(i);
+                        w.WriteLine(']');
+                        w.Write("login = ");
+                        w.WriteLine(acc.Login);
+                        w.Write("password = ");
+                        w.WriteLine(acc.Password);
+                        w.Write("access = ");
+                        w.WriteLine((int) acc.Access);
+                        i++;
+                    }
                 }
             }
         }
