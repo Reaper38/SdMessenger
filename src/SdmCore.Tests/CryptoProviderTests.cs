fec4c09c55fb49e3e9b96994f19f385df98816cf
nitrocaster <paul-kv@yandex.ru>
2015-05-01 21:04:56 +0300

    Remove redundant usings.

--- src/SdmCore.Tests/CryptoProviderTests.cs
+++ src/SdmCore.Tests/CryptoProviderTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
 using System.Threading;
 using NUnit.Framework;
 using Sdm.Core.Crypto.Detail;
