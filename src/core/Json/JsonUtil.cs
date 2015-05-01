4448138cf94eaafe5192aecc90aed7d308c6b982
nitrocaster <paul-kv@yandex.ru>
2015-05-01 18:59:20 +0300

    Fix formatting.

--- src/core/Json/JsonUtil.cs
+++ src/core/Json/JsonUtil.cs
@@ -14,9 +14,7 @@ namespace Sdm.Core.Json
     public class JsonStreamReader : JsonTextReader
     {
         public JsonStreamReader(Stream s, bool buffered = true)
-            : base(buffered ? (TextReader) new StreamReader(s) : (TextReader) new UnbufferedStreamReader(s))
-        {
-            Unbuffered = !buffered;
-        }
+            : base(buffered ? (TextReader)new StreamReader(s) : new UnbufferedStreamReader(s))
+        { Unbuffered = !buffered; }
     }
 }
