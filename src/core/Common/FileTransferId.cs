f8e0f68cfaf87831d9a5c64bfe31d1c531d57097
nitrocaster <paul-kv@yandex.ru>
2015-05-04 18:30:14 +0300

    Add file transfer session id struct.

---
+++ src/core/Common/FileTransferId.cs
@@ -0,0 +1,34 @@
+﻿namespace Sdm.Core
+{
+    /// <summary>Unique file transfer session id (provided by server).</summary>
+    public struct FileTransferId
+    {
+        public readonly uint Value;
+
+        public FileTransferId(uint value)
+        { Value = value; }
+
+        public static bool operator ==(FileTransferId a, FileTransferId b)
+        { return a.Value == b.Value; }
+
+        public static bool operator !=(FileTransferId a, FileTransferId b)
+        { return !(a == b); }
+
+        public int CompareTo(FileTransferId other)
+        { return Value.CompareTo(other.Value); }
+
+        public bool Equals(FileTransferId other)
+        { return Value == other.Value; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            return obj is FileTransferId && Equals((FileTransferId)obj);
+        }
+
+        public override int GetHashCode() { return Value.GetHashCode(); }
+
+        public override string ToString() { return Value.ToString(); }
+    }
+}
