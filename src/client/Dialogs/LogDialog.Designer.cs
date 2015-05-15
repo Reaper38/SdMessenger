f1112ea0a7e1640f6d49c0aec8c52d995347a000
nitrocaster <paul-kv@yandex.ru>
2015-05-16 02:29:11 +0300

    Add log dialog.

---
+++ src/client/Dialogs/LogDialog.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Sdm.Client
+{
+    partial class LogDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xrConsole = new xr.Console();
+            this.SuspendLayout();
+            // 
+            // xrConsole
+            // 
+            this.xrConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.xrConsole.Font = new System.Drawing.Font("Consolas", 9F);
+            this.xrConsole.Location = new System.Drawing.Point(0, 0);
+            this.xrConsole.Name = "xrConsole";
+            this.xrConsole.Size = new System.Drawing.Size(624, 282);
+            this.xrConsole.TabIndex = 0;
+            // 
+            // LogDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 282);
+            this.Controls.Add(this.xrConsole);
+            this.Name = "LogDialog";
+            this.Text = "Log";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private xr.Console xrConsole;
+    }
+}
---
+++ src/client/Dialogs/LogDialog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using Sdm.Client.Controls;
+
+namespace Sdm.Client
+{
+    public partial class LogDialog : FormEx
+    {
+        public LogDialog()
+        {
+            InitializeComponent();
+            xrConsole.CommandLineEnabled = false;
+        }
+
+        public xr.Console Console
+        { get { return xrConsole; } }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            e.Cancel = true;
+            base.OnClosing(e);
+            Hide();
+        }
+    }
+}
