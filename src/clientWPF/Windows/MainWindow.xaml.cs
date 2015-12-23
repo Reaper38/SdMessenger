2d6804b220d6485ca5932c4766b2ab4d5ff68e41
Reaper38 <spiritgame38@gmail.com>
2015-12-23 04:06:00 +0300

    added frame to main window

--- src/clientWPF/Windows/MainWindow.xaml.cs
+++ src/clientWPF/Windows/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SdmClientWPF
+namespace Sdm.ClientWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
